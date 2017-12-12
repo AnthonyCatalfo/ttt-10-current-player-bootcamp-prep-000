5968932e0e10feac7ac6ed13c40c7581546db874
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-12-12 08:44:29 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,6 +2,6 @@ def turn_count(board)
     board.reject { |item| item.nil? || item == '' || item==" " }
     return board.length
 end
-def current_player
+def current_player(board)
   return turn_count(board) % 2==0 ? 'O' : 'X'
 end
